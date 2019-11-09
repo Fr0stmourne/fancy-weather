0ca0230b495dc7beb404c8562eea6d80e74b7d59
Fr0stmourne <cozroman@yandex.ru>
2019-11-09 15:14:58 +0300

    feat: implement a pencil tool

--- palette/gulpfile.js
+++ palette/gulpfile.js
@@ -1,133 +1,114 @@
-const del = require(`del`);
-const gulp = require(`gulp`);
-const sass = require(`gulp-sass`);
-const plumber = require(`gulp-plumber`);
-const postcss = require(`gulp-postcss`);
-const autoprefixer = require(`autoprefixer`);
-const server = require(`browser-sync`).create();
-const mqpacker = require(`css-mqpacker`);
-const minify = require(`gulp-csso`);
-const rename = require(`gulp-rename`);
-const imagemin = require(`gulp-imagemin`);
-const svgstore = require(`gulp-svgstore`);
-const tinypng = require(`gulp-tinypng-compress`);
-const svgmin = require(`gulp-svgmin`);
-const rollup = require(`gulp-better-rollup`);
-const sourcemaps = require(`gulp-sourcemaps`);
+const del = require('del');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+const mqpacker = require('css-mqpacker');
+const minify = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const tinypng = require('gulp-tinypng-compress');
+const svgmin = require('gulp-svgmin');
+const rollup = require('gulp-better-rollup');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task(`style`, () => {
-  return gulp.src(`src/sass/style.scss`, {
-    allowEmpty: true
-  }).
-  pipe(plumber()).
-  pipe(sass()).
-  pipe(postcss([
+gulp.task('style', () => gulp.src('src/sass/style.scss', {
+  allowEmpty: true,
+})
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(postcss([
     autoprefixer({}),
     mqpacker({
-      sort: true
-    })
-  ])).
-  pipe(gulp.dest(`build/css`)).
-  pipe(server.stream()).
-  pipe(minify()).
-  pipe(rename(`style.min.css`)).
-  pipe(gulp.dest(`build/css`));
-});
+      sort: true,
+    }),
+  ]))
+  .pipe(gulp.dest('build/css'))
+  .pipe(server.stream())
+  .pipe(minify())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('build/css')));
 
-gulp.task(`sprite`, function () {
-  return gulp.src(`./build/**/*.svg`)
-    .pipe(svgmin({
-      plugins: [{
-        removeViewBox: false
-      }]
-    }))
-    // .pipe(svgstore({
-    //   inlineSvg: true
-    // }))
-    .pipe(rename(`sprite.svg`))
-    .pipe(gulp.dest(`./build`));
-});
+gulp.task('sprite', () => gulp.src('./build/**/*.svg')
+  .pipe(svgmin({
+    plugins: [{
+      removeViewBox: false,
+    }],
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('./build')));
 
-gulp.task(`scripts`, () => {
-  return gulp.src(`src/js/**/*.js`, {
-      allowEmpty: true
-    })
-    .pipe(plumber())
-    .pipe(sourcemaps.init())
-    .pipe(rollup({}, `iife`))
-    .pipe(sourcemaps.write(``))
-    .pipe(gulp.dest(`build/js`));
-});
+gulp.task('scripts', () => gulp.src('src/js/**/*.js', {
+  allowEmpty: true,
+})
+  .pipe(plumber())
+  .pipe(sourcemaps.init())
+  .pipe(rollup({}, 'iife'))
+  .pipe(sourcemaps.write(''))
+  .pipe(gulp.dest('build/js')));
 
 
-gulp.task(`copy`, gulp.series(`scripts`, `style`, () => {
-  return gulp.src([
-    `src/**/*.{html,ico}`,
-    `src/**/*.{ttf,eot,woff,woff2}`,
-    `src/**/*.{jpg,svg,png,jpeg,gif}`,
-  ], {
+gulp.task('copy', gulp.series('scripts', 'style', () => gulp.src([
+  'src/**/*.{html,ico}',
+  'src/**/*.{ttf,eot,woff,woff2}',
+  'src/**/*.{jpg,svg,png,jpeg,gif}',
+], {
 
-  }).
-  pipe(gulp.dest(`build`));
-}));
+})
+  .pipe(gulp.dest('build'))));
 
 
-
-gulp.task(`tinypng`, gulp.series(async () => {
-  return gulp.src(`build/img/**/*.{jpg,png,jpeg,gif}`)
-    .pipe(tinypng({
-      key: `RsCZev1geyFwOKznstLNGmxugsTZZmG6`,
-      // sigFile: `./img/.tinypng-sigs`,
-      // log: true
-    }))
-    .pipe(gulp.dest(`build/img`));
-}));
+gulp.task('tinypng', gulp.series(async () => gulp.src('build/img/**/*.{jpg,png,jpeg,gif}')
+  .pipe(tinypng({
+    key: 'RsCZev1geyFwOKznstLNGmxugsTZZmG6',
+    // sigFile: `./img/.tinypng-sigs`,
+    // log: true
+  }))
+  .pipe(gulp.dest('build/img'))));
 
 
-gulp.task(`imagemin`, gulp.series(() => {
-  return gulp.src(`build/img/**/*.{jpg,png,jpeg,gif}`).
-  pipe(imagemin([
+gulp.task('imagemin', gulp.series(() => gulp.src('build/img/**/*.{jpg,png,jpeg,gif}')
+  .pipe(imagemin([
     imagemin.optipng({
-      optimizationLevel: 3
+      optimizationLevel: 3,
     }),
     imagemin.jpegtran({
-      progressive: true
-    })
-  ])).
-  pipe(gulp.dest(`build/img`));
-}));
+      progressive: true,
+    }),
+  ]))
+  .pipe(gulp.dest('build/img'))));
 
 
-gulp.task(`clean`, () => {
-  return del(`build`);
-});
+gulp.task('clean', () => del('build'));
 
-gulp.task(`reload`, (done) => {
+gulp.task('reload', (done) => {
   server.reload();
   done();
 });
 
-gulp.task(`js-watch`, gulp.series(`scripts`, (done) => {
+gulp.task('js-watch', gulp.series('scripts', (done) => {
   server.reload();
   done();
 }));
 
-gulp.task(`assemble`, gulp.series(`clean`, `copy`));
+gulp.task('assemble', gulp.series('clean', 'copy'));
 
 // imagemin/tinypng img compression
 
-gulp.task(`build`, gulp.series(`assemble`, `imagemin`, `tinypng`, `sprite`));
+gulp.task('build', gulp.series('assemble', 'imagemin', 'tinypng', 'sprite'));
 
-gulp.task(`serve`, gulp.series(`build`, () => {
+gulp.task('serve', gulp.series('build', () => {
   server.init({
-    server: `./build`,
+    server: './build',
     notify: false,
     open: true,
     port: 3502,
-    ui: false
+    ui: false,
   });
 
-  gulp.watch(`src/sass/**/*.{scss,sass}`, gulp.series(`style`, `reload`));
-  gulp.watch(`src/*.html`, gulp.series(`copy`, `reload`));
-  gulp.watch(`src/js/**/*.js`, gulp.series(`js-watch`));
+  gulp.watch('src/sass/**/*.{scss,sass}', gulp.series('style', 'reload'));
+  gulp.watch('src/*.html', gulp.series('copy', 'reload'));
+  gulp.watch('src/js/**/*.js', gulp.series('js-watch'));
 }));
--- palette/src/js/index.js
+++ palette/src/js/index.js
@@ -1,86 +1,46 @@
-const canvas = document.getElementById(`canvas`);
-const ctx = canvas.getContext(`2d`);
-const btn4x4 = document.getElementById(`4x4`);
-const btn32x32 = document.getElementById(`32x32`);
-const imageBtn = document.getElementById(`image`);
-const labels = document.querySelectorAll(`.picture-controls__label`);
-
-const activeClass = `picture-controls__label--active`;
-
-btn4x4.addEventListener(`change`, (e) => {
-  [...labels].forEach((el) => el.classList.remove(activeClass));
-  e.target.closest(`label`).classList.add(activeClass);
-  fetch(btn4x4.dataset.link)
-    .then((response) => response.json())
-    .then((matrix) => {
-      drawImageFromMatrix(matrix);
-    });
-});
-
-btn32x32.addEventListener(`change`, (e) => {
-  [...labels].forEach((el) => el.classList.remove(activeClass));
-  e.target.closest(`label`).classList.add(activeClass);
-  fetch(btn32x32.dataset.link)
-    .then((response) => response.json())
-    .then((matrix) => {
-      drawImageFromMatrix(matrix);
-    });
-});
-
-imageBtn.addEventListener(`change`, (e) => {
-  [...labels].forEach((el) => el.classList.remove(activeClass));
-  e.target.closest(`label`).classList.add(activeClass);
-  drawImageFromPic(imageBtn.dataset.link);
-});
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const controls = document.querySelectorAll('.controls__control-btn');
+const controlsList = controls[0].closest('ul');
+// const labels = document.querySelectorAll('.picture-controls__label');
+const pixelSize = 128;
+const activeClass = 'controls__control-btn--active';
 
 
-function drawImageFromMatrix(colorMatrix) {
-  const canvasSize = colorMatrix.length;
-  canvas.width = canvasSize;
-  canvas.height = canvasSize;
+const fillColor = 'green';
+canvas.width = 4;
+canvas.height = 4;
 
-  function convertHexToRGBA(hexString) {
-    const resultArr = [];
-    for (let i = 0; i < 6; i += 2) {
-      resultArr.push(parseInt(hexString.slice(i, i + 2), 16));
-    }
-    resultArr.push(255);
-    return resultArr;
-  }
-
-  function convertColorMatrix(arr) {
-    let hasToBeConverted = true;
-    if (typeof arr[0][0] !== `string`) {
-      hasToBeConverted = !hasToBeConverted;
-    }
-    return arr.reduce((acc, row) => {
-      acc.push(row.reduce((rgbaArr, el) => {
-        rgbaArr.push(new ImageData(new Uint8ClampedArray(hasToBeConverted ? convertHexToRGBA(el) : el), 1, 1));
-        return rgbaArr;
-      }, []));
-      return acc;
-    }, []);
-  }
-
-  const imageArr = convertColorMatrix(colorMatrix);
-
-  for (let dy = 0; dy < canvasSize; dy++) {
-    for (let dx = 0; dx < canvasSize; dx++) {
-      ctx.putImageData(imageArr[dy][dx], dx, dy);
-    }
-  }
+function getMousePos(evt) {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.floor((evt.clientX - rect.left) / pixelSize),
+    y: Math.floor((evt.clientY - rect.top) / pixelSize),
+  };
 }
 
-function drawImageFromPic(link) {
-  const img = new Image();
-  img.addEventListener(`load`, drawImg);
-  img.src = `${link}`;
-
-  function drawImg() {
-    canvas.width = this.width;
-    canvas.height = this.height;
-    ctx.drawImage(this, 0, 0);
-  }
+function pressedMouseMoveHandler(evt) {
+  const {
+    x,
+    y,
+  } = getMousePos(evt);
+  ctx.fillStyle = fillColor;
+  ctx.fillRect(x, y, 1, 1);
 }
 
-btn4x4.click();
+canvas.addEventListener('mousedown', (evt) => {
+  canvas.addEventListener('mousemove', pressedMouseMoveHandler);
+
+  const coordinates = getMousePos(evt);
+  ctx.fillStyle = fillColor;
+  ctx.fillRect(coordinates.x, coordinates.y, 1, 1);
+});
+
+canvas.addEventListener('mouseup', () => {
+  canvas.removeEventListener('mousemove', pressedMouseMoveHandler);
+});
+
+controlsList.addEventListener('click', (e) => {
+  controls.forEach((el) => el.classList.remove(activeClass));
+  e.target.classList.add(activeClass);
+});
