1266fa66f9694972625dea07ce88204236dafff9
Fr0stmourne <cozroman@yandex.ru>
2019-11-09 22:32:41 +0300

    feat: implement color picker and add keyboard shortcuts support(although there are several bugs related to them)

--- palette/src/js/index.js
+++ palette/src/js/index.js
@@ -3,28 +3,40 @@ const controlMode = new Map([
   [1, 'color'],
   [2, 'pencil']
 ]);
+const hotkeyBindings = {
+  'KeyB': 'fill',
+  'KeyP': 'pencil',
+  'KeyC': 'color'
+}
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const controls = document.querySelectorAll('.controls__control-btn');
 const controlsList = controls[0].closest('ul');
-const colorInput = document.querySelector('input[type="color"]'); // need to remove second input
+const colorInput = document.querySelector('input[type="color"]');
+const colorInputLbl = colorInput.closest('label');
+const prevColorBtn = document.querySelector('.colors__color--previous .colors__color-btn');
+let img = new Image();
 // const labels = document.querySelectorAll('.picture-controls__label');
 const pixelSize = 128;
 const activeClass = 'controls__control-btn--active';
-
 let mode = 'fill';
 
+function switchMode(newMode) {
+  if (newMode === undefined) throw new Error('Unknown app mode.');
+  mode = newMode;
+}
+
+let prevColor = 'lightgreen';
+let lastColor;
 
 let fillColor = colorInput.value;
-console.log(fillColor);
 canvas.width = 512 / pixelSize;
 canvas.height = 512 / pixelSize;
 
 function getMousePos(evt) {
-  const rect = canvas.getBoundingClientRect();
   return {
-    x: Math.floor((evt.clientX - rect.left) / pixelSize),
-    y: Math.floor((evt.clientY - rect.top) / pixelSize),
+    x: Math.floor(evt.offsetX / pixelSize),
+    y: Math.floor(evt.offsetY / pixelSize),
   };
 }
 
@@ -37,8 +49,18 @@ function pressedMouseMoveHandler(evt) {
   ctx.fillRect(x, y, 1, 1);
 }
 
-canvas.addEventListener('mousedown', (evt) => {
 
+function updateControls(e, evtCode) {
+  controls.forEach((el) => el.classList.remove(activeClass));
+  if (evtCode) {
+    const key = controlsList.querySelector(`*[data-key=${evtCode}]`);
+    if (key) key.classList.add(activeClass);
+  } else {
+    e.target.classList.add(activeClass);
+  }
+}
+
+canvas.addEventListener('mousedown', (evt) => {
   if (mode === 'pencil') {
     canvas.addEventListener('mousemove', pressedMouseMoveHandler);
     const coordinates = getMousePos(evt);
@@ -52,7 +74,17 @@ canvas.addEventListener('mousedown', (evt) => {
       ctx.fillRect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
     })
   }
-
+  if (mode === 'color') {
+    const colorClickHandler = (evt) => {
+      const color = ctx.getImageData(getMousePos(evt).x, getMousePos(evt).y, 1, 1).data.slice(0, 3);
+      prevColorBtn.style.backgroundColor = fillColor;
+      colorInput.value = rgbToHex(`rgb(${color.join(',')}`);
+      updateColor();
+      controls[2].click();
+      canvas.removeEventListener('click', colorClickHandler);
+    }
+    canvas.addEventListener('click', colorClickHandler);
+  }
 });
 
 canvas.addEventListener('mouseup', () => {
@@ -61,22 +93,46 @@ canvas.addEventListener('mouseup', () => {
   }
 });
 
-controlsList.addEventListener('click', (e) => {
-  controls.forEach((el) => el.classList.remove(activeClass));
-  e.target.classList.add(activeClass);
-});
+controlsList.addEventListener('click', (e) => updateControls(e));
 
 controls.forEach((control, index) => {
-  control.addEventListener('click', () => {
-    mode = controlMode.get(index);
-  })
+  control.addEventListener('click', (e) => {
+    switchMode(controlMode.get(index));
+  });
 })
 
 
-colorInput.addEventListener('change', () => {
 
+document.addEventListener('keydown', (evt) => {
+  switchMode(hotkeyBindings[evt.code]);
+  updateControls(evt, evt.code);
+})
+
+
+colorInput.addEventListener('change', (e) => {
+  prevColorBtn.style.backgroundColor = fillColor;
+  updateColor();
+});
+
+function componentToHex(c) {
+  var hex = (+c).toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(str) {
+  const rgbArr = str.split("(")[1].split(")")[0].split(',');
+  return "#" + rgbArr.reduce((acc, el) => acc += componentToHex(el.trim()), '');
+}
+
+function updateColor() {
   fillColor = colorInput.value;
-  console.log(fillColor);
-})
+  colorInputLbl.style.backgroundColor = colorInput.value;
+}
 
 controls[2].click();
+prevColorBtn.style.backgroundColor = '#90ee90';
+
+prevColorBtn.addEventListener('click', () => {
+  [colorInput.value, prevColorBtn.style.backgroundColor] = [rgbToHex(prevColorBtn.style.backgroundColor), colorInput.value];
+  updateColor();
+})
