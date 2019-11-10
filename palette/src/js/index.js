ca015f7fe748af0b2d6b348b38b3fb2427be3514
Fr0stmourne <cozroman@yandex.ru>
2019-11-10 13:30:18 +0300

    feat: add bresenham algorithm

--- palette/src/js/index.js
+++ palette/src/js/index.js
@@ -1,13 +1,13 @@
 const controlMode = new Map([
   [0, 'fill'],
   [1, 'color'],
-  [2, 'pencil']
+  [2, 'pencil'],
 ]);
 const hotkeyBindings = {
-  'KeyB': 'fill',
-  'KeyP': 'pencil',
-  'KeyC': 'color'
-}
+  KeyB: 'fill',
+  KeyP: 'pencil',
+  KeyC: 'color',
+};
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const controls = document.querySelectorAll('.controls__control-btn');
@@ -15,18 +15,18 @@ const controlsList = controls[0].closest('ul');
 const colorInput = document.querySelector('input[type="color"]');
 const colorInputLbl = colorInput.closest('label');
 const prevColorBtn = document.querySelector('.colors__color--previous .colors__color-btn');
-let img = new Image();
 // const labels = document.querySelectorAll('.picture-controls__label');
-const pixelSize = 128;
+const pixelSize = 4;
 const activeClass = 'controls__control-btn--active';
 let mode = 'fill';
+
 
 function switchMode(newMode) {
   if (newMode === undefined) throw new Error('Unknown app mode.');
   mode = newMode;
 }
 
-let prevColor = 'lightgreen';
+const prevColor = 'lightgreen';
 let lastColor;
 
 let fillColor = colorInput.value;
@@ -40,13 +40,43 @@ function getMousePos(evt) {
   };
 }
 
+function line(x0, y0, x1, y1) {
+  const dx = Math.abs(x1 - x0);
+  const sx = x0 < x1 ? 1 : -1;
+  const dy = Math.abs(y1 - y0);
+  const sy = y0 < y1 ? 1 : -1;
+  let err = (dx > dy ? dx : -dy) / 2;
+
+  while (true) {
+    ctx.fillRect(x0, y0, 1, 1);
+    if (x0 === x1 && y0 === y1) break;
+    const e2 = err;
+    if (e2 > -dx) {
+      err -= dy;
+      x0 += sx;
+    }
+    if (e2 < dy) {
+      err += dx;
+      y0 += sy;
+    }
+  }
+}
+
+let lastCoords = {};
+
 function pressedMouseMoveHandler(evt) {
   const {
     x,
     y,
   } = getMousePos(evt);
+  lastCoords = lastCoords.x ? lastCoords : {
+    x,
+    y,
+  };
   ctx.fillStyle = fillColor;
-  ctx.fillRect(x, y, 1, 1);
+  line(lastCoords.x, lastCoords.y, x, y);
+  lastCoords.x = x;
+  lastCoords.y = y;
 }
 
 
@@ -61,16 +91,17 @@ function updateControls(e, evtCode) {
 }
 
 canvas.addEventListener('mousedown', (evt) => {
-
   if (mode === 'pencil') {
     canvas.addEventListener('mousemove', pressedMouseMoveHandler);
     const coordinates = getMousePos(evt);
     ctx.fillStyle = fillColor;
     ctx.fillRect(coordinates.x, coordinates.y, 1, 1);
 
-    canvas.addEventListener('mouseleave', () => {
+    canvas.addEventListener('mouseleave', (e) => {
       canvas.removeEventListener('mousemove', pressedMouseMoveHandler);
-    })
+      // line(lastCoords.x, lastCoords.y, e.offsetX, e.offsetY);
+      lastCoords = {};
+    });
   }
 
   if (mode === 'fill') {
@@ -78,7 +109,7 @@ canvas.addEventListener('mousedown', (evt) => {
       ctx.fillStyle = fillColor;
       ctx.fillRect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
       canvas.removeEventListener('click', fillHandler);
-    }
+    };
     canvas.addEventListener('click', fillHandler);
   }
   if (mode === 'color') {
@@ -89,26 +120,27 @@ canvas.addEventListener('mousedown', (evt) => {
       updateColor();
       controls[2].click();
       canvas.removeEventListener('click', colorClickHandler);
-    }
+    };
     canvas.addEventListener('click', colorClickHandler);
   }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  let dataURL = localStorage.getItem('canvasData');
+  const dataURL = localStorage.getItem('canvasData');
   if (dataURL) {
-    let img = new Image;
+    const img = new Image();
     img.src = dataURL;
     img.onload = () => {
       ctx.drawImage(img, 0, 0);
     };
   }
-})
+});
 
 canvas.addEventListener('mouseup', () => {
   if (mode === 'pencil') {
     canvas.removeEventListener('mousemove', pressedMouseMoveHandler);
     localStorage.setItem('canvasData', canvas.toDataURL());
+    lastCoords = {};
   }
 });
 
@@ -118,14 +150,13 @@ controls.forEach((control, index) => {
   control.addEventListener('click', (e) => {
     switchMode(controlMode.get(index));
   });
-})
-
+});
 
 
 document.addEventListener('keydown', (evt) => {
   switchMode(hotkeyBindings[evt.code]);
   updateControls(evt, evt.code);
-})
+});
 
 
 colorInput.addEventListener('change', (e) => {
@@ -134,13 +165,13 @@ colorInput.addEventListener('change', (e) => {
 });
 
 function componentToHex(c) {
-  var hex = (+c).toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+  const hex = (+c).toString(16);
+  return hex.length == 1 ? `0${hex}` : hex;
 }
 
 function rgbToHex(str) {
-  const rgbArr = str.split("(")[1].split(")")[0].split(',');
-  return "#" + rgbArr.reduce((acc, el) => acc += componentToHex(el.trim()), '');
+  const rgbArr = str.split('(')[1].split(')')[0].split(',');
+  return `#${rgbArr.reduce((acc, el) => acc += componentToHex(el.trim()), '')}`;
 }
 
 function updateColor() {
@@ -154,4 +185,4 @@ prevColorBtn.style.backgroundColor = '#90ee90';
 prevColorBtn.addEventListener('click', () => {
   [colorInput.value, prevColorBtn.style.backgroundColor] = [rgbToHex(prevColorBtn.style.backgroundColor), colorInput.value];
   updateColor();
-})
+});
